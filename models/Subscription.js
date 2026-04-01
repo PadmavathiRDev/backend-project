05bfdfdab547cac779d9069454330c5163bcdef5
PadmavathiRDev <Rpadmavathi@thestackly.com>
2026-04-01 09:15:37 +0530

    Add files via upload

---
+++ models/Subscription.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  userId: String,
+  plan: String,
+  paymentId: String,
+  status: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Subscription", schema);
