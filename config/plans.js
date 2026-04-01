d305d003fc7887379a5fc0c75e370dac7446c586
PadmavathiRDev <Rpadmavathi@thestackly.com>
2026-04-01 09:13:31 +0530

    Add files via upload

---
+++ config/plans.js
@@ -0,0 +1,15 @@
+const PLANS = {
+  BASIC: {
+    name: "Basic",
+    amount: 4000
+  },
+  BUSINESS: {
+    name: "Business",
+    amount: 15000
+  },
+  ADVANCED: {
+    name: "Advanced",
+    amount: 18000
+  }
+};
+module.exports = PLANS;
