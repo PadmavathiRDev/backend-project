2150cb68515fc170043a7eaffacb8de0747cdf6b
PadmavathiRDev <Rpadmavathi@thestackly.com>
2026-04-01 09:14:08 +0530

    Add files via upload

---
+++ controllers/paymentController.js
@@ -0,0 +1,42 @@
+const { createOrderService } = require("../services/paymentService");
+const verifySignature = require("../utils/verifySignature");
+const Subscription = require("../models/subscription");
+
+exports.createOrder = async (req, res) => {
+  try {
+    const { plan } = req.body;
+
+    const { order, plan: selectedPlan } =
+      await createOrderService(plan);
+
+    res.json({
+      orderId: order.id,
+      amount: selectedPlan.amount,
+      plan: selectedPlan.name
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.verifyPayment = async (req, res) => {
+  try {
+    const isValid = verifySignature(req.body);
+
+    if (!isValid) {
+      return res.status(400).json({ success: false });
+    }
+
+    // Save in DB
+    await Subscription.create({
+      userId: "demoUser", // replace with auth user
+      plan: req.body.plan,
+      paymentId: req.body.razorpay_payment_id,
+      status: "SUCCESS"
+    });
+
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
