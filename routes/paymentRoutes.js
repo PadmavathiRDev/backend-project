c1476d9c929c071879a2986e0244f773e02b5fa1
PadmavathiRDev <Rpadmavathi@thestackly.com>
2026-04-15 11:03:11 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,5 +3,6 @@ const controller = require("../controllers/paymentController");
 
 router.post("/create-order", controller.createOrder);
 router.post("/verify", controller.verifyPayment);
+router.get("/invoice/:id", controller.downloadInvoice);
 
 module.exports = router;
